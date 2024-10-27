576cda557f7b55d4f29c3b4824307086f3b2d381
ErAshishChawla <ashishchawladev@gmail.com>
2024-10-27 16:21:41 -0400

    Tickets initial commit

---
+++ tickets/src/index.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+import { keys } from "./lib/keys";
+
+const start = async () => {
+  const MAX_RETRIES = 5;
+  const { mongoURI } = keys;
+
+  // Check if all the required environment variables are set.
+  Object.values(keys).forEach((value) => {
+    if (!value) {
+      throw new Error("Missing environment variable");
+    }
+  });
+
+  for (let i = 0; i < MAX_RETRIES; i++) {
+    try {
+      await mongoose.connect(mongoURI);
+      mongoose.connection.on("error", (error) => {
+        console.log("Connection to MongoDB failed: ", error);
+      });
+      console.log("Connected to MongoDB...");
+      break;
+    } catch (error) {
+      console.log("Error connecting to MongoDB: ", error);
+      console.log("Retrying in 5 seconds...");
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
---
+++ tickets/src/lib/keys.ts
@@ -0,0 +1,3 @@
+export const keys = {
+  mongoURI: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+};
